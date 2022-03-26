d984a3a7103df76158b25bdc54b6a86fc7b5f247
abieiden <abbi.lop33@gmail.com>
2022-03-26 04:09:57 -0500

    working toggle, fixed nav bar, started resume page

--- my-app/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navigation } from "./navigation";
-export { default as Footer } from "./footer";
-export { default as Home } from "./home";
-export { default as About } from "./about";
-export { default as Contact } from "./contact";
